770769146b5c0f30afa0cbed1f594a042efabdd3
Andreas Nurbo <andreas.nurbo@gmail.com>
2016-05-04 23:27:16 +0200

    add nginx dispatch fix, closes #11

--- generators/app/templates/box/scripts/provision.sh
+++ generators/app/templates/box/scripts/provision.sh
@@ -14,6 +14,10 @@ if [ ! -f "${wp_file}" ]; then
     echo " "
 fi
 
+sudo sed -i -e 's/root \/var\/www;/root \/var\/www\/$http_host;/g' /etc/nginx/sites-available/default
+grep -q -F 'location /content/ {alias /var/www/$http_host/content/;}'  /etc/nginx/global/wordpress.conf || echo 'location /content/ {alias /var/www/$http_host/content/;}' | sudo tee -a  /etc/nginx/global/wordpress.conf
+sudo service nginx restart
+
 #
 # Create all sites
 #
